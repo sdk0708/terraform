c03797ef72438f0dbefc2c0cec19405d0160e338
carpediembs <tjdehdrms94@naver.com>
2021-11-16 14:34:27 +0900

    step3

--- 01_terraform module/intall.sh
+++ 01_terraform module/intall.sh
@@ -9,7 +9,7 @@ cp -a ./wordpress/* /var/www/html/
 chown -R apache.apache /var/www/html/
 sed -i 's/DirectoryIndex index.html/DirectoryIndex index.php/g' /etc/httpd/conf/httpd.conf
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sed -i 's/database_name_here/test/g' /var/www/html/wp-config.php
+sed -i 's/database_name_here/mydb/g' /var/www/html/wp-config.php
 sed -i 's/username_here/admin/g' /var/www/html/wp-config.php
 sed -i 's/password_here/It12345!/g' /var/www/html/wp-config.php
 sed -i 's/localhost/mydb.cqep7uwq9udb.ap-northeast-2.rds.amazonaws.com/g' /var/www/html/wp-config.php
